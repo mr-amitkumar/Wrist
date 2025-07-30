d679f28151c99d501dce2bafec353c31772f6626
Amit Kumar Mahapatra <157622482+mr-amitkumar@users.noreply.github.com>
2025-07-30 15:15:10 +0530

    amit

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,15 +1,19 @@
 import Home from "./HomePage/Home";
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 const App = () => {
   return (
     <div>
+    <Router>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes> 
       <Footer />
+    </Router>
     </div>
   )
 }
